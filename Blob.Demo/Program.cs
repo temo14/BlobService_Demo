8be8065e28c421211d48a70d490e713ab22f5baf
Tem0B <Temo.baindurashvili@universaltransit.us>
2023-02-20 19:10:01 +0400

    add .

---
+++ Blob.Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Blob.Demo.Service;
+using Blob.Demo.Service.Contract;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection();
+
+services.AddTransient<IBlobService, BlobService>();
